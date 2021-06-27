15b9f50914a3288c1d89c97d9791abbafd172881
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-28 04:12:06 +0530

    Create Bloc class

--- lib/src/screens/bloc/home_bloc.dart
+++ lib/src/screens/bloc/home_bloc.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class HomeBloc extends Bloc<BlockEventBase, HomeState> {
+  @override
+  Stream<HomeState> mapEventToState(BlockEventBase event) async* {
+    if (event is ChangeRedLightEvent) {
+      yield HomeState(isLightOn: true);
+    } else if (event is ChangeYellowLightEvent) {
+      yield HomeState(isLightOn: true);
+    } else if (event is ChangeGreenLightEvent) {
+      yield HomeState(isLightOn: true);
+    }
+  }
+}
+
+class HomeState {
+  final bool isLightOn;
+
+  HomeState({
+    @required this.isLightOn,
+  });
+}
+
+abstract class BlockEventBase {}
+
+class ChangeRedLightEvent extends BlockEventBase {}
+
+class ChangeYellowLightEvent extends BlockEventBase {}
+
+class ChangeGreenLightEvent extends BlockEventBase {}
